d14bf62adf136ab55e98f0a5bf3692a7e6de8269
vovapidjak <nekononov@gmail.com>
2025-05-18 12:40:05 +0300

    методы строк и списков, повторение + множества

---
+++ повторениее.py
@@ -0,0 +1,33 @@
+# .count()
+# вызывается от списков и строк
+s = 'hello world'
+# print(s.count('l'))
+
+#.index()
+# print(s.index('l'))
+# for i in range(len(s)):
+#     print(s[i])
+
+# # .replace()
+# s = s.replace('l', '1', 2)
+# print(s)
+#
+# lst = ['11', '12', '13', 11, 11]
+# print(lst.count('11'))
+#
+# print(len('1234'))
+
+# дана строка s = 'hello world'
+# в списке указать индексы букв 'l'
+# lst = []
+# for i in range(len(s)):
+#     if s[i] == 'l':
+#         lst.append(i)
+# print(lst)
+
+s = 'hello world'
+# срезы
+# строка[начало:конец:шаг]
+# print(s[4:-1])
+print(s[::-1])
+# print(bin(123)[2:])
